ab615b5edfa562b4659c6712ee00cf8e420a3c90
Jagan Baskar <jackjagan1995@gmail.com>
2024-05-02 16:48:13 +0530

    Added comment on the value annotation

--- src/main/java/com/wheatherprojectbyjagan/weatherproject/Services/WeatherService.java
+++ src/main/java/com/wheatherprojectbyjagan/weatherproject/Services/WeatherService.java
@@ -8,6 +8,8 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class WeatherService {
 
+//    @value will fetch the information from application properties file and will update the variables with value given in properties files
+
 //    @Value("${openWeather.url-cityTemp}")
     private String BaseURLCityTemp;
 //    @Value("${openWeather.token}")
